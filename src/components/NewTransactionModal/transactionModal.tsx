b83a685d2b7934aeae95b534e8ddaec5effd67d0
Tiago <ti.almeida58@yahoo.com>
2023-10-09 14:13:17 -0300

    feat: button and styled-component function

--- src/components/NewTransactionModal/transactionModal.tsx
+++ src/components/NewTransactionModal/transactionModal.tsx
@@ -1,6 +1,10 @@
 import Modal from 'react-modal';
-import { Container } from './style';
+import { Container, TransactionTypeContainer, RadioBox } from './style';
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { useState } from 'react';
+
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,6 +12,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
     return (
         <Modal 
           isOpen={isOpen}
@@ -34,6 +40,26 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     type='number'
                     placeholder='Valor'
                 />
+
+                <TransactionTypeContainer>
+                    <RadioBox 
+                        type="button"
+                        onClick={() => { setType('deposit'); }}
+                        isActive={type === 'deposit'}
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox 
+                        type="button"
+                        onClick={() => { setType('withdraw'); }}
+                        isActive={type === 'withdraw'}
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
 
                 <input
                     placeholder='Categoria'
