43e9b8f78c92d042251cfe176fe5778ce7f12827
Tiago-Silva <ti.almeida58@gmail.com>
2023-10-10 09:01:28 -0300

    feat: creating context provider

--- src/components/TransactionsTable/transacationTable.tsx
+++ src/components/TransactionsTable/transacationTable.tsx
@@ -1,24 +1,10 @@
 
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Container } from './style';
-import { api } from '../services/api';
-
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
-}
+import { TransactionsContext } from '../../contexts/TransactionContext';
 
 export function TransactionsTable() {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-    useEffect(() => {
-        api.get('transactions')
-            .then(response => setTransactions(response.data.transactions));
-    }, []);
+    const transactions = useContext(TransactionsContext);
 
     return (
         <Container>
