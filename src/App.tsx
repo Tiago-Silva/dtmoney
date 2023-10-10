e7952c508831c83ab65e0c3d2feba5df22345ac0
Tiago <ti.almeida58@yahoo.com>
2023-10-10 09:01:28 -0300

    feat: creating context provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header/header";
 import { GlobalStyle } from "./styles/global";
 import Modal from 'react-modal';
 import { NewTransactionModal } from "./components/NewTransactionModal/transactionModal";
+import { TransactionsProvider } from "./contexts/TransactionContext";
 
 Modal.setAppElement('#root');
 
@@ -20,7 +21,7 @@ export function App() {
 
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
@@ -30,6 +31,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/Summary/summary.tsx
+++ src/components/Summary/summary.tsx
@@ -1,10 +1,14 @@
-import { Container } from "./style";
+import { useContext } from 'react';
+import { Container } from './style';
 import IncomeImg from '../../assets/income.svg';
 import OutcommeImg from '../../assets/outcome.svg';
 import TotalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../contexts/TransactionContext';
 
 
 export function Summary() {
+    const transactions = useContext(TransactionsContext);
+
     return (
         <Container>
             <div>
